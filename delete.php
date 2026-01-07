0e72fcbef18ad0e3e69a3c8be1b5d7cc8f5a5838
David-cds <dapasma12@gmail.com>
2026-01-07 14:04:29 +0100

    delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,13 @@
 <?php
 require_once("./session.php");
 
-// Supongamos que el ID del producto a eliminar viene de un formulario
-$id = $_POST['id']; // o $_GET['id'] dependiendo de cómo lo recibas
 
-// Validar que sea un número para seguridad
+$id = $_POST['id']; 
+
+
 $id = intval($id);
 
-// Query DELETE
+
 $sql = "DELETE FROM producto WHERE id = $id";
 
 if ($conn->query($sql) === TRUE) {
